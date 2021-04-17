f6e44ea59afbd7103850c6cc825ee2e70c96b678
Eray Bartan <ebartan@gmail.com>
2021-04-17 13:07:24 +0300

    odalar design two

--- pages/odalar.js
+++ pages/odalar.js
@@ -12,7 +12,7 @@ export default function Odalar() {
       <div className="relative h-64 m-2 overflow-hidden rounded-lg bg-olive-300">
   <div className="absolute z-30 flex w-full h-full">
     <div className="relative z-30 w-5/6 px-6 py-8 text-white md:py-10 md:w-1/2">
-      <h2 className="text-4xl">Bol ışık alan odalarımızın her biri, konforlu bir konaklama imkanına sahiptir.</h2><span></span>
+      <h2 className="text-3xl">Bol ışık alan odalarımızın her biri, konforlu bir konaklama imkanına sahiptir.</h2><span></span>
     </div>
     <div className="absolute top-0 right-0 flex w-full h-full">
       <div className="w-1/3 h-full bg-odalar"></div>
@@ -28,14 +28,15 @@ export default function Odalar() {
 </div>
 <div><p className=" text-xl ml-2 mr-2 mt-2 mb-2">Konfor tek hedefimiz değil, aynı zamanda tasarıma ve çağdaş mobilyalara da değer veriyoruz.
 Odalarımızın deniz manzaralı pencerelerinden ve teraslarından görülebilen zengin doğa paletini görebilirsiniz.</p>
-<Image className="ml-2 mr-2 mt-2 mb-2"
+<div className="ml-2 mr-2 mt-2 mb-2 animate-pulse"><Image 
         src="/odalar-cover.jpg"
         alt="Picture of the author"
         width={820}
         height={312}
-      />
+      /></div>
+
 </div>
-<div><p className="text-2xl ml-2 mr-2 mt-2 mb-2">Yüksel Butik Hotel 4 farklı oda tipi vardır. Single, Double, Triple ve Aile şeklinde toplam 12 oda ve 30 yatak bulunmaktadır. Odalarımızın kullanım alanı 16 m2 ile 24 m2 arasında değişmektedir.</p></div>
+<div><p className="text-xl ml-2 mr-2 mt-2 mb-2">Yüksel Butik Hotel 4 farklı oda tipi vardır. Single, Double, Triple ve Aile şeklinde toplam 12 oda ve 30 yatak bulunmaktadır. </p><p className="text-xl ml-2 mr-2 mt-2 mb-2">Odalarımızın kullanım alanı 16 m2 ile 24 m2 arasında değişmektedir.</p></div>
       </Layout>
   )
 }
